9bb435ebe8c2113ecd24c5b8dc9e38b9cea3764c
Daniel Kimani <daniel.kibunga@students.moringaschool.com>
2026-05-01 22:37:32 +0300

    just starting out

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("taskInput");
+
+const addButton = document.getElementById("addButton");
+
+const tasklist = document.getElementById("taskList");
+
+let tasks = JSON.parse
